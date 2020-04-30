bf30524e3fd76730fa4f894230aacc30b27bfe5a
khakholin <khakholin@mail.ru>
2020-04-30 19:34:46 +0700

    [FC]: add useLocalStorage hook, optiont with cookies, request to server app

--- src/utils/endingForNumber.ts
+++ src/utils/endingForNumber.ts
@@ -1,20 +1,22 @@
-const endingForNumber = (num: number): string => {
+const endingForNumber = (num: number | undefined): string => {
     let ending = '';
     const exceptions = [11, 12, 13, 14, 15, 16, 17, 18, 19];
     const secondGroup = [2, 3, 4];
     const thirdGroup = [5, 6, 7, 8, 9, 0];
 
-    if (exceptions.find((item) => item === num)) {
-        ending = 'й';
-    } else {
-        if (num % 10 === 1) {
-            ending = 'я';
-        }
-        if (secondGroup.find((item) => item === num % 10) !== undefined) {
-            ending = 'и';
-        }
-        if (thirdGroup.find((item) => item === num % 10) !== undefined) {
+    if (num) {
+        if (exceptions.find((item) => item === num)) {
             ending = 'й';
+        } else {
+            if (num % 10 === 1) {
+                ending = 'я';
+            }
+            if (secondGroup.find((item) => item === num % 10) !== undefined) {
+                ending = 'и';
+            }
+            if (thirdGroup.find((item) => item === num % 10) !== undefined) {
+                ending = 'й';
+            }
         }
     }
 
