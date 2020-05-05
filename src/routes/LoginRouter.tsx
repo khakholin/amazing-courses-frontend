e0cfabdbd11ee26e89b6588ce4a379a869f8293c
khakholin <khakholin@mail.ru>
2020-05-05 17:17:49 +0700

    [FCF]: fix private router with token, commented before starting work

--- src/routes/LoginRouter.tsx
+++ src/routes/LoginRouter.tsx
@@ -1,9 +1,20 @@
 import React, { FC } from 'react';
 import { Redirect, Route } from 'react-router-dom';
+
 import Login from '../pages/Login/Login';
+import { getCookieByName } from '../utils/operationsWithCookie';
+
 import { RoutePath } from './constants/routesConstants';
 
 const LoginRouter: FC = () => {
+    // const token = getCookieByName('auth');
+
+    // if (token && Object.keys(token).length) {
+    //     return <Redirect to={RoutePath.personalArea} />;
+    // } else {
+    //     return <Route component={Login} path={RoutePath.login} exact />;
+    // }
+
     const isLogin = false;
 
     if (isLogin) {
