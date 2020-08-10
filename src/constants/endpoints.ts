66c894784baddaea38f7e74862ff8021873a9b25
khakholin <khakholin@mail.ru>
2020-08-10 10:40:15 +0700

    fix api url

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL || 'http://localhost:8080';
+export const API_URL = process.env.API_URL || 'http://khakholin.ru';
 
 export const endpoints = {
     authLogin: '/api/user/auth/login',
