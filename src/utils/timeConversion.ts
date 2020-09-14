5ca72fa8561b6fa9d05b392c3aa12d2441e4483a
khakholin <khakholin@mail.ru>
2020-09-14 12:50:10 +0700

    update time conversion fundtion

--- src/utils/timeConversion.ts
+++ src/utils/timeConversion.ts
@@ -1,18 +1,11 @@
-const timeConversion = (seconds: number | undefined): string => {
-    let time = '';
-    if (seconds) {
-        if (seconds > 59) {
-            if (seconds > 3599) {
-                time = (Math.floor(seconds / 3600) >= 10 ? Math.floor(seconds / 3600) : '0' + Math.floor(seconds / 3600)) + ':' + ((Math.floor((seconds - 3600) / 60) >= 10 ? (Math.floor((seconds - 3600) / 60)) : '0' + (Math.floor((seconds - 3600) / 60))) + ':' + ((seconds - 3600) % 60 >= 10 ? (seconds - 3600) % 60 : '0' + (seconds - 3600) % 60));
+import moment from 'moment';
 
-            } else {
-                time = '00:' + (Math.floor(seconds / 60) >= 10 ? Math.floor(seconds / 60) : '0' + Math.floor(seconds / 60)) + ':' + (seconds % 60 >= 10 ? seconds % 60 : '0' + seconds % 60);
-            }
-        } else {
-            time = '00:00:' + (seconds >= 10 ? seconds : '0' + seconds);
-        }
+const timeConversion = (seconds: number | undefined): string => {
+    if (seconds) {
+        return moment().startOf('day').seconds(seconds).format('HH:mm:ss');
+    } else {
+        return '';
     }
-    return time;
 }
 
 export default timeConversion;
